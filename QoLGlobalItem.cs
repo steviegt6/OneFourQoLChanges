aa2d80f8f3bf7197b0bec178c685bdcb0f69d09e
Stevie <xxlennygamerxx@gmail.com>
2020-06-10 21:39:58 -0700

    Paint, Actuators, and Ale as ammo

---
+++ QoLGlobalItem.cs
@@ -0,0 +1,69 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OneFourQoLChanges
+{
+    public class QoLGlobalItem : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            switch (item.type)
+            {
+                case ItemID.RedPaint:
+                case ItemID.OrangePaint:
+                case ItemID.YellowPaint:
+                case ItemID.LimePaint:
+                case ItemID.GreenPaint:
+                case ItemID.TealPaint:
+                case ItemID.CyanPaint:
+                case ItemID.SkyBluePaint:
+                case ItemID.BluePaint:
+                case ItemID.PurplePaint:
+                case ItemID.VioletPaint:
+                case ItemID.PinkPaint:
+                case ItemID.BlackPaint:
+                case ItemID.GrayPaint:
+                case ItemID.WhitePaint:
+                case ItemID.BrownPaint:
+                case ItemID.ShadowPaint:
+                case ItemID.NegativePaint:
+                case ItemID.DeepRedPaint:
+                case ItemID.DeepOrangePaint:
+                case ItemID.DeepYellowPaint:
+                case ItemID.DeepLimePaint:
+                case ItemID.DeepGreenPaint:
+                case ItemID.DeepTealPaint:
+                case ItemID.DeepCyanPaint:
+                case ItemID.DeepSkyBluePaint:
+                case ItemID.DeepBluePaint:
+                case ItemID.DeepPurplePaint:
+                case ItemID.DeepVioletPaint:
+                case ItemID.DeepPinkPaint:
+                    if (QoLModConfig.Instance.paintAmmoSlot)
+                        item.ammo = ItemID.RedPaint;
+                    else
+                        item.ammo = AmmoID.None;
+                    break;
+                case ItemID.Actuator:
+                    if (QoLModConfig.Instance.actuatorsAmmoSlot)
+                        item.ammo = ItemID.Actuator;
+                    else
+                        item.ammo = AmmoID.None;
+                    break;
+                case ItemID.Ale:
+                    if (QoLModConfig.Instance.aleAmmoSlot)
+                    {
+                        item.ammo = ItemID.Ale;
+                        item.notAmmo = false;
+                    }
+                    else
+                    {
+                        item.ammo = AmmoID.None;
+                        item.notAmmo = true;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ QoLModConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace OneFourQoLChanges
+{
+    [Label("1.4 QoL Mod Config")]
+    public class QoLModConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        public static QoLModConfig Instance;
+
+        [Header("Ammo Slot Changes")]
+        [Label("Allow Paint to be Stored in Ammo Slots")]
+        [DefaultValue(true)]
+        [ReloadRequired]
+        public bool paintAmmoSlot;
+
+        [Label("Allow Actuators to be Stored in Ammo Slots")]
+        [DefaultValue(true)]
+        [ReloadRequired]
+        public bool actuatorsAmmoSlot;
+
+        [Label("Allow Ale to be Stored in Ammo Slots")]
+        [DefaultValue(true)]
+        [ReloadRequired]
+        public bool aleAmmoSlot;
+    }
+}
